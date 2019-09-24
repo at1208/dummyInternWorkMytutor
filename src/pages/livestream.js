7d58f386f75efccdbd813b5178902327e7f67079
at1208 <amantiwari12081999@gmail.com>
2019-09-24 14:59:00 +0530

    cors proxy done

--- src/pages/livestream.js
+++ src/pages/livestream.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button,Card,CardBody,CardHeader,Col,Form,FormFeedback,FormGroup,FormText,Input,Label,Row,} from 'reactstrap';
+import {Button,Card,CardBody,CardHeader,Col,Form ,FormGroup ,Input,Label,} from 'reactstrap';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Flash from 'react-reveal/Flash';
@@ -9,17 +9,59 @@ import Flash from 'react-reveal/Flash';
 
 class LiveStream extends Component {
 
+
   state = {
    EditLive: 0,
    LiveList: 0,
-   startDate: new Date() };
+   startDate: new Date(),
+   isLoaded: false,
+   token: ''
+
+  };
+
+
+   componentDidMount() {
+  const email = "demo_admin@gmail.com";
+  const password = "123123";
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = "http://ec2-13-127-45-88.ap-south-1.compute.amazonaws.com:4000/users/loginDasboard";
+
+
+  fetch(proxyurl + url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email,
+      password
+    })
+  })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw Error(res.statusText);
+      }
+    })
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        token: json
+      });
+    })
+    .catch(error => console.error(error));
+}
+
+
 
  handleChange = date => {
    this.setState({
      startDate: date }); };
 
 render(){
-
+console.log(this.state.token)
  if(this.state.LiveList){
    return<div>
 
